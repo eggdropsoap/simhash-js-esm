08e1ea3a9c3fc5bf080cab3cf3bdb80f5ae3876e
eggdropsoap <eggdropsoap@pipemaze.com>
2025-01-05 21:43:43 -0800

    Enhance tests

--- test/ComparatorTest.js
+++ test/ComparatorTest.js
@@ -15,9 +15,13 @@ describe('Comparator', function() {
         it('should find similarity in very similar short inputs', function() {
             const simHash = new SimHash();
             const threshold = 0.5;
+            const msg = {
+                a: 'test message',
+                b: ' test message'
+            }
 
-            const h1 = simHash.hash('test message?');
-            const h2 = simHash.hash('test message!');
+            const h1 = simHash.hash(msg.a);
+            const h2 = simHash.hash(msg.b);
             const comp = Comparator.similarity(h1,h2);
 
             assert(comp > threshold);
@@ -26,9 +30,13 @@ describe('Comparator', function() {
         it('should not find similarity in very different short inputs', function() {
             const simHash = new SimHash();
             const threshold = 0.5;
+            const msg = {
+                a: 'test message',
+                b: 'mock parrots'
+            }
 
-            const h1 = simHash.hash('test message');
-            const h2 = simHash.hash('mock-parrot-argument is an obligator carnivor');
+            const h1 = simHash.hash(msg.a);
+            const h2 = simHash.hash(msg.b);
             const comp = Comparator.similarity(h1,h2);
 
             assert(comp < threshold);
@@ -37,9 +45,12 @@ describe('Comparator', function() {
         it('should find similarity in very similar medium inputs', function() {
             const simHash = new SimHash();
             const threshold = 0.7;
-
-            const h1 = simHash.hash("This is a test of the Emergency Blogcast System");
-            const h2 = simHash.hash("This is a second test of the Emergency Blogcast System");    
+            const msg = {
+                a: "This is a test of the Emergency Blogcast System",
+                b: "This is a second test of the Emergency Blogcast System"
+            }
+            const h1 = simHash.hash(msg.a);
+            const h2 = simHash.hash(msg.b);
             const comp = Comparator.similarity(h1,h2);
 
             assert(comp > threshold);
